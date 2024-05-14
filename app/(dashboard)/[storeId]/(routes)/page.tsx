e171e586c596f620482404ee2abc5bac3ecad309
noyes5 <noyes5@naver.com>
2024-05-14 16:51:48 +0900

    feat: Navbar 생성 및 store 컴포넌트 연결

--- app/(dashboard)/[storeId]/(routes)/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -1,22 +1,17 @@
 import prismadb from "@/lib/prismadb";
 
 interface DashboardPageProps {
-  params: { storeId: string}
+  params: { storeId: string };
 }
 
-const DashboardPage: React.FC<DashboardPageProps> = async ({
-  params
-}) => {
+const DashboardPage: React.FC<DashboardPageProps> = async ({ params }) => {
   const store = await prismadb.store.findFirst({
     where: {
-      id: params.storeId
-    }
+      id: params.storeId,
+    },
+  });
 
-  })
-  
-  return (<div>
-    Active Store: {store?.name}
-  </div>);
+  return <div>Active Store: {store?.name}</div>;
 };
 
 export default DashboardPage;
