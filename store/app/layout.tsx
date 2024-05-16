c6aeb8703b0c2db67a4f5f3fa20da9d1e54a0c18
noyes5 <noyes5@naver.com>
2024-05-16 14:00:30 +0900

    feat: ecommerce-store first commit

---
+++ store/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Urbanist } from "next/font/google";
+import "./globals.css";
+
+const font = Urbanist({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Store",
+  description: "Store",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+
+    </html>
+  );
+}
