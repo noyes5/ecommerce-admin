9d7baa8366b1526bc846fd46c108ff00f23f44dc
noyes5 <noyes5@naver.com>
2024-05-16 13:15:51 +0900

    feat: Product 엔티티 생성

---
+++ app/(dashboard)/[storeId]/(routes)/products/components/cell-action.tsx
@@ -0,0 +1,88 @@
+"use client";
+
+import axios from "axios";
+import { Copy, Edit, MoreHorizontal, Trash } from "lucide-react";
+import toast from "react-hot-toast";
+import { useParams, useRouter } from "next/navigation";
+import { useState } from "react";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Button } from "@/components/ui/button";
+import { AlertModal } from "@/components/modals/alert-modal";
+
+import { ProductColumn } from "./columns";
+
+interface CellActionProps {
+  data: ProductColumn;
+}
+
+export const CellAction: React.FC<CellActionProps> = ({ data }) => {
+  const router = useRouter();
+  const params = useParams();
+
+  const [loading, setLoading] = useState(false);
+  const [open, setOpen] = useState(false);
+
+  const onCopy = (id: string) => {
+    navigator.clipboard.writeText(id);
+    toast.success("상품 ID가 복사되었습니다.");
+  };
+
+  const onDelete = async () => {
+    try {
+      setLoading(true);
+      await axios.delete(`/api/${params.storeId}/billboards/${data.id}`);
+      router.refresh();
+      toast.success("상품 삭제됨");
+    } catch (error) {
+      toast.error("Something went wrong.");
+    } finally {
+      setLoading(false);
+      setOpen(false);
+    }
+  };
+
+  return (
+    <>
+      <AlertModal
+        isOpen={open}
+        onClose={() => setOpen(false)}
+        onConfirm={onDelete}
+        loading={loading}
+      />
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="ghost" className="h-8 w-8 p-0">
+            <span className="sr-only">Open menu</span>
+            <MoreHorizontal className="h-4 w-4" />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end">
+          <DropdownMenuLabel>실행</DropdownMenuLabel>
+          <DropdownMenuItem onClick={() => onCopy(data.id)}>
+            <Copy className="mr-2 h-4 w-4" />
+            아이디 복사하기
+          </DropdownMenuItem>
+          <DropdownMenuItem
+            onClick={() =>
+              router.push(`/${params.storeId}/products/${data.id}`)
+            }
+          >
+            <Edit className="mr-2 h-4 w-4" />
+            업데이트 하기
+          </DropdownMenuItem>
+          <DropdownMenuItem onClick={() => setOpen(true)}>
+            <Trash className="mr-2 h-4 w-4" />
+            삭제
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </>
+  );
+};
