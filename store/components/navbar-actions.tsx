adac8d161562422d7a28106548bd863d422bb530
noyes5 <noyes5@naver.com>
2024-05-17 17:22:53 +0900

    feat: cart 페이지 구현

--- store/components/navbar-actions.tsx
+++ store/components/navbar-actions.tsx
@@ -2,8 +2,10 @@
 
 import { ShoppingBag } from "lucide-react";
 import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
 
 import Button from "@/components/ui/button";
+import useCart from "@/hooks/use-cart";
 
 const NavbarActions = () => {
   const [isMounted, setIsMounted] = useState(false);
@@ -12,15 +14,20 @@ const NavbarActions = () => {
     setIsMounted(true);
   }, []);
 
+  const router = useRouter();
+  const cart = useCart();
+
   if (!isMounted) {
     return null;
   }
 
   return (
     <div className="ml-auto flex itmes-center gap-x-4">
-      <Button className="flex items-center rounded-full bgblack px-4 py-2">
+      <Button onClick={() => router.push("/cart")} className="flex items-center rounded-full bgblack px-4 py-2">
         <ShoppingBag size={20} color="white" />
-        <span className="ml-2 text-sm font-medium text-white">0</span>
+        <span className="ml-2 text-sm font-medium text-white">
+          {cart.items.length}
+        </span>
       </Button>
     </div>
   );
